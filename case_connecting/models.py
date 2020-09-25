be1e3882a7e41e1d89bd362ae9e5b916b09a9810
Paul Rodriguez <prod00@me.com>
2020-09-25 19:06:58 -0400

    Database connected with SQL. tested a few dummy posts to see if it was working.

--- case_connecting/models.py
+++ case_connecting/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Post(models.Model):
+    # things to add: deadline, how many people applied
+    recruiter = models.ForeignKey(User, on_delete=models.CASCADE)
+    position = models.CharField(max_length=200)
+    knowledge = models.CharField(max_length=200)
+    content = models.TextField()
+    pay = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.position
--- case_connecting/views.py
+++ case_connecting/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'recruiter': 'Paul Rodriguez',
-        'position': 'Website Designer',
-        'knowledge': 'HTML/CSS',
-        'content': 'Working on a website for dogs and I need someone to design it for me',
-        'pay': '$50 once completed',
-        'date_posted': '9/25/2020'
-    },
-    {
-        'recruiter': 'Dr. Koyuturk ',
-        'position': 'RA',
-        'knowledge': 'Python Django or Flask',
-        'content': 'Survey website collecting data on which language people speak',
-        'pay': '$15 an hour',
-        'date_posted': '9/25/2020'
-    }
-]
 
 #home page for case_connecting
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'case_connecting/home.html', context)
 
