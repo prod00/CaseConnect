f556e7ad3211c404d896c7378fbb9b7c94dd9670
Meira <mvf8@case.edu>
2020-10-24 13:38:00 -0400

    Moved styling to css in static folder and fixed Filter box to only be on the Home page. Started Activity page

--- CaseConnect/settings.py
+++ CaseConnect/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
-import django
-django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
